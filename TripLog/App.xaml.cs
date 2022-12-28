8e5562f499b335d694f90864bb99d58e15165edf
Katherine Hambley <superdesigngirl@mac.com>
2022-12-28 12:01:44 -0600

    Added MainPage with hardcoded list of LogEntries

--- TripLog/App.xaml.cs
+++ TripLog/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TripLog.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace TripLog
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart ()
---
+++ TripLog/Views/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using TripLog.Models;
+using Xamarin.Forms;
+
+namespace TripLog.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var items = new List<TripLogEntry>
+            {
+                new TripLogEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing!",
+                    Rating = 3,
+                    Date = new DateTime(2019, 2, 5),
+                    Latitude = 38.8895,
+                    Longitude = -77.0352
+                },
+                new TripLogEntry
+                {
+                    Title = "Statue of Liberty",
+                    Notes = "Inspiring!",
+                    Rating = 4,
+                    Date = new DateTime(2019, 4, 13),
+                    Latitude = 40.6892,
+                    Longitude = -74.0444
+                },
+                new TripLogEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy, but beautiful.",
+                    Rating = 5,
+                    Date = new DateTime(2019, 4, 26),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
+                }
+            };
+            trips.ItemsSource = items;
+        }
+    }
+}
+
