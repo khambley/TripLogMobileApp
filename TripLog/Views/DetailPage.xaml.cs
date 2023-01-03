4c6df9b0ba88ba1532b9d264d870b95ba246d0c9
Katherine Hambley <superdesigngirl@mac.com>
2023-01-03 11:29:09 -0600

    Hooked Maps control into DetailViewModel PropertyChanged event.

--- TripLog/Views/DetailPage.xaml.cs
+++ TripLog/Views/DetailPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using TripLog.Models;
 using TripLog.Services;
 using TripLog.ViewModels;
@@ -15,21 +16,51 @@ namespace TripLog.Views
         {
             InitializeComponent();
             BindingContext = new DetailViewModel(DependencyService.Get<INavService>());
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if(ViewModel != null)
+            {
+                ViewModel.PropertyChanged += OnViewModelPropertyChanged;
+            }
+        }
 
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if(ViewModel != null)
+            {
+                ViewModel.PropertyChanged -= OnViewModelPropertyChanged;
+            }
+        }
+
+        void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == nameof(DetailViewModel.Entry))
+            {
+                UpdateMap();
+            }
+        }
+
+        void UpdateMap()
+        {
+            //Center the map around the log entry's location
             map.MoveToRegion(MapSpan.FromCenterAndRadius(
                 new Position(
                     ViewModel.Entry.Latitude,
                     ViewModel.Entry.Longitude),
                     Distance.FromMiles(.5)));
 
+            //Place a pin on the map for the log entry's location
             map.Pins.Add(new Pin
             {
                 Type = PinType.Place,
                 Label = ViewModel.Entry.Title,
                 Position = new Position(ViewModel.Entry.Latitude, ViewModel.Entry.Longitude)
             });
-
         }
+
     }
 }
 
